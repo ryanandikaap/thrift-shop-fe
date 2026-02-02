bf7f2292c595b0a3d4ba2153db28543da5279937
ryanandikaprathama <ryanandikaprathama@gmail.com>
2026-02-02 07:33:24 +0700

    kategori

--- src/pages/info/Category.jsx
+++ src/pages/info/Category.jsx
@@ -12,7 +12,7 @@ const categoryDetails = {
   },
   'Sepatu': {
     icon: <Footprints size={36} />,
-    image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ab?ixlib=rb-4.0.3&auto=format&fit=crop&w=870&q=80',
+    image: 'https://images.unsplash.com/photo-1622760806364-5ccac8096b59?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   'Aksesoris': {
     icon: <Watch size={36} />,
